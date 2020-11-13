aeb40fc597fa508c0ed77b73d7949eb7f3418e00
tasankuluulu <tasankuluulu@gmail.com>
2020-11-12 22:41:59 -0500

    updated the path to configuration.properties file

--- src/test/java/com/hrms/utils/Constants.java
+++ src/test/java/com/hrms/utils/Constants.java
@@ -2,7 +2,7 @@ package com.hrms.utils;
 
 public class Constants {
 
-	public static final String CONFIGURATION_FILEPATH = System.getProperty("user.dir") + "\\src\\test\\resources\\configs\\configuration.properties";
+	public static final String CONFIGURATION_FILEPATH = System.getProperty("user.dir") + "/src/test/resources/configs/configuration.properties";
 	public static final String TESTDATA_FILEPATH = System.getProperty("user.dir") + "\\src\\test\\resources\\testdata\\HrmsTestData.xlsx";
 	public static final String EXCELFILE_PATH = System.getProperty("user.dir") + "\\src\\test\\resources\\testdata\\HrmsTestData.xlsx";
 	public static final String SCREENSHOT_FILEPATH = System.getProperty("user.dir") + "\\target\\screenshots\\";
